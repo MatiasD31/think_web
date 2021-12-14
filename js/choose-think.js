9e9278f9b4a58b56d20bfe7e9754a73ee0602d57
MatiasD31 <88781940+MatiasD31@users.noreply.github.com>
2021-12-14 13:54:36 -0300

    Orden de components , paginas nuevas

--- js/choose-think.js
+++ js/choose-think.js
@@ -1,10 +1,12 @@
+
 function load_choose_think()
 {
     btns_razons = document.getElementsByClassName('btns-razons')
 
+    box_razons = document.getElementsByClassName('razons')
+
     for( i = 0 ; i < btns_razons.length ; i++ )
     {
-        box_razons = document.getElementsByClassName('razons')
 
         btns_razons[i].addEventListener('click',action)
     }
@@ -12,13 +14,23 @@ function load_choose_think()
 
 function action(e) 
 {
-    for( i = 0 ; i < btns_razons.length ; i++ ) box_razons[i].style.display = 'none'
+    for( i = 0 ; i < btns_razons.length ; i++ ) 
+    {
+
+        box_razons[i].style.display = 'none'
+
+        btns_razons[i].style.backgroundColor = 'transparent'
+
+    }
 
     index = e.currentTarget.getAttribute('for')
 
     console.log( index )
 
     box_razons[ index ].style.display = 'flex'
+
+    btns_razons[ index ].style.backgroundColor = '#FFFFFF'
+
 }
 
 
